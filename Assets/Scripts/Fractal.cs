e471b8f0d8e9063c42419c0bd90376beff329800
Reid Blomquist <reid@theprogrampdx.com>
2015-07-10 15:01:12 -0700

    init commit

---
+++ Assets/Scripts/Fractal.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh mesh;
+	public Material material;
+	public int maxDepth;
+	public float childScale;
+
+	private int depth;
+
+	private void Initialize (Fractal parent, Vector3 direction) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+		transform.rotation = parent.transform.rotation;
+	}
+
+	private void Start () {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+	
+	private IEnumerator CreateChildren () {
+		yield return new WaitForSeconds(0.5f);
+			new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.up);
+		yield return new WaitForSeconds(0.5f);
+			new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, Vector3.right);
+	}
+}
