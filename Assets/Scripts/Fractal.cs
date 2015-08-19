16e8652998e17cf24f9c1f41d40ccbf8de5c3f5c
Reid Blomquist <reid.blomquist@gmail.com>
2015-08-18 19:23:23 -0700

    switching to list for binarysearchability, bringing in VS tools

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -12,6 +12,7 @@ public class Fractal : MonoBehaviour {
 	private int depth;
 	private Material[,] materials;
 	private static int childCount;
+	private static int wat;
 
 	private static Vector3[] childDirections = {
 		Vector3.up,
@@ -21,7 +22,7 @@ public class Fractal : MonoBehaviour {
 		Vector3.back
 	};
 
-	private static int[] childThresholds = new int[childDirections.Length + 1];
+	private static List<int> childThresholds = new List<int> { };
 
 	private static Quaternion[] childOrientations = {
 		Quaternion.identity,
@@ -40,7 +41,9 @@ public class Fractal : MonoBehaviour {
 
 	private void Initialize (Fractal parent, int childIndex) {
 		childCount++;
-		Debug.Log(childCount);
+		int index = childThresholds.BinarySearch(childCount);
+		int highest = index < 0 ? ~index : index;
+		int result = childThresholds[highest];
 		mesh = parent.mesh;
 		materials = parent.materials;
 		maxDepth = parent.maxDepth;
@@ -69,9 +72,9 @@ public class Fractal : MonoBehaviour {
 	private void InitializeThreshholds () {
 		for (int i = 0; i < (childDirections.Length + 1); i++) {
 			if (i == 0) {
-				childThresholds[i] = 1;
+				childThresholds.Insert(i, 1);
 			} else {
-				childThresholds[i] = 5 * childThresholds[i - 1] + 1;
+				childThresholds.Insert(i, (5 * childThresholds[i - 1] + 1));
 			}
 		}
 	}
@@ -80,9 +83,13 @@ public class Fractal : MonoBehaviour {
 		if (materials == null) {
 			InitializeMaterials();
 		}
+		if (childThresholds.Count == 0)
+		{
+			InitializeThreshholds();
+		}
 		gameObject.AddComponent<MeshFilter>().mesh = mesh;
 		gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range (0, 2)];
-		InitializeThreshholds();
+
 		if (depth < maxDepth) {
 			StartCoroutine(CreateChildren());
 		}
