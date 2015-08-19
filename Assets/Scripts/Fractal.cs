8f4e04710db34aaecb7a016137472023a3e0972f
Reid Blomquist <reid.blomquist@gmail.com>
2015-08-19 14:43:18 -0700

    starting to apply force to layers/waves; randomize between different force directions?

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -2,17 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Fractal : MonoBehaviour {
+public class Fractal : MonoBehaviour
+{
 
 	public Mesh mesh;
 	public Material material;
 	public int maxDepth;
 	public float childScale;
+	public float thrust;
 
 	private int depth;
 	private Material[,] materials;
 	private static int childCount;
-	private static int wat;
+	private static int depthResult;
+	private GameObject[] layerObjects;
 
 	private static Vector3[] childDirections = {
 		Vector3.up,
@@ -32,19 +35,24 @@ public class Fractal : MonoBehaviour {
 		Quaternion.Euler (-90f, 0f, 0f)
 	};
 
-	private IEnumerator CreateChildren () {
-		for (int i = 0; i < childDirections.Length; i++) {
-			yield return new WaitForSeconds(Random.Range (0.1f, 0.5f));
-			new GameObject("Fractal Child").AddComponent<Fractal>().Initialize (this, i);
+	private IEnumerator CreateChildren()
+	{
+		for (int i = 0; i < childDirections.Length; i++)
+		{
+			yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+			new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, i);
 		}
 	}
 
-	private void Initialize (Fractal parent, int childIndex) {
+	private void Initialize(Fractal parent, int childIndex)
+	{
 		childCount++;
 		int index = childThresholds.BinarySearch(childCount);
 		int highest = index < 0 ? ~index : index;
-		int result = childThresholds[highest];
-		tag = result.ToString();
+		depthResult = childThresholds[highest];
+		tag = depthResult.ToString();
+		Rigidbody sr = this.gameObject.AddComponent<Rigidbody>();
+		sr = parent.GetComponent<Rigidbody>();
 		mesh = parent.mesh;
 		materials = parent.materials;
 		maxDepth = parent.maxDepth;
@@ -57,27 +65,34 @@ public class Fractal : MonoBehaviour {
 
 	}
 
-	private void InitializeMaterials () {
+	private void InitializeMaterials()
+	{
 		materials = new Material[maxDepth + 1, 2];
-		for (int i = 0; i <= maxDepth; i++) {
+		for (int i = 0; i <= maxDepth; i++)
+		{
 			float t = i / (maxDepth - 1f);
 			t *= t;
 			materials[i, 0] = new Material(material);
-			materials[i, 0].color = Color.Lerp (Color.white, Color.yellow, t);
+			materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
 			materials[i, 1] = new Material(material);
-			materials[i, 1].color = Color.Lerp (Color.white, Color.cyan, t);
+			materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
 		}
 		materials[maxDepth, 0].color = Color.magenta;
 		materials[maxDepth, 1].color = Color.red;
 	}
 
-	private void InitializeThreshholds () {
-		for (int i = 0; i < (childDirections.Length + 1); i++) {
-			if (i == 0) {
+	private void InitializeThreshholds()
+	{
+		for (int i = 0; i < (childDirections.Length + 1); i++)
+		{
+			if (i == 0)
+			{
 				int threshold = 1;
 				childThresholds.Insert(i, threshold);
 				TagHelper.AddTag(threshold.ToString());
-			} else {
+			}
+			else
+			{
 				int threshold = (5 * childThresholds[i - 1] + 1);
 				childThresholds.Insert(i, threshold);
 				TagHelper.AddTag(threshold.ToString());
@@ -85,18 +100,37 @@ public class Fractal : MonoBehaviour {
 		}
 	}
 
-	private void Start () {
-		if (materials == null) {
+	private void BlowupLayer()
+	{
+		layerObjects = GameObject.FindGameObjectsWithTag(depthResult.ToString());
+
+		foreach (GameObject item in layerObjects)
+		{
+			Rigidbody rb = item.GetComponent<Rigidbody>();
+			rb.AddForce(transform.forward * thrust);
+		}
+	}
+
+	private void Start()
+	{
+		if (materials == null)
+		{
 			InitializeMaterials();
 		}
 		if (childThresholds.Count == 0)
 		{
 			InitializeThreshholds();
 		}
-		gameObject.AddComponent<MeshFilter>().mesh = mesh;
-		gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range (0, 2)];
+		if (childCount == depthResult)
+		{
+			BlowupLayer();
+		}
 
-		if (depth < maxDepth) {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range(0, 2)];
+
+		if (depth < maxDepth)
+		{
 			StartCoroutine(CreateChildren());
 		}
 	}
